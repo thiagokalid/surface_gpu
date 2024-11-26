0a2f3b9db3ffa16d5f9c68bd8b883328920da213
Thiago Kalid <thiagokkalid@gmail.com>
2024-11-26 09:25:02 -0300

    interpolated version

--- src/surface_gpu/methods.py
+++ src/surface_gpu/methods.py
@@ -26,6 +26,102 @@ def parrilla_2007(surface, focus, transmitter, c1, c2):
             k0 = int(k)
 
 
+def parrilla_generalized_interpolated(x: list, z: list, N:list, xA: float, zA: float, xF: float, zF: float, c: list, tolerance: int=2, maxiter: int=100):
+    M = len(c)
+    k0 = np.zeros(M-1, dtype=float)
+    step = np.copy(k0)
+    k = k0 + 1000
+    elapsed_time = None
+
+    output = {
+        "ki": [],
+        "elapsed_time": -1,
+        "converged": False,
+        "result": None,
+        "iter": 0
+    }
+
+    t0 = time.time()
+    for i in range(maxiter):
+        for m in range(M-1):  # 0 1 2
+            if m == 0:
+                p = k0[m]
+                pn = k0[m + 1]
+                Mk = (z[m](p + 1) - z[m](p)) / (x[m](p + 1) - x[m](p))
+
+                Vk0 = \
+                    1 / c[0] * ((x[m](p) - xA) + Mk * (z[m](p) - zA)) / np.sqrt(
+                        (x[m](p) - xA) ** 2 + (z[m](p) - zA) ** 2) + \
+                    1 / c[1] * ((x[m](p) - x[m + 1](pn)) + Mk * (z[m](p) - z[m + 1](pn))) / np.sqrt(
+                        (x[m](p) - x[m + 1](pn)) ** 2 + (z[m](p) - z[m + 1](pn)) ** 2)
+
+                Vk = \
+                    1 / c[0] * ((x[m](p + 1) - xA) + Mk * (z[m](p + 1) - zA)) / np.sqrt(
+                        (x[m](p + 1) - xA) ** 2 + (z[m](p + 1) - zA) ** 2) + \
+                    1 / c[1] * ((x[m](p + 1) - x[m + 1](pn)) + Mk * (z[m](p + 1) - z[m + 1](pn))) / np.sqrt(
+                        (x[m](p + 1) - x[m + 1](pn)) ** 2 + (z[m](p + 1) - z[m + 1](pn)) ** 2)
+
+                step[0] = Vk0 / (Vk - Vk0)
+
+            elif m == M - 2:
+                p0 = k0[m - 1]
+                p = k0[m]
+                Mk = (z[m](p + 1) - z[m](p)) / (x[m](p + 1) - x[m](p))
+
+                Vk0 = \
+                    1 / c[-2] * ((x[m](p) - x[m - 1](p0)) + Mk * (z[m](p) - z[m - 1](p0))) / np.sqrt(
+                        (x[m](p) - x[m - 1](p0)) ** 2 + (z[m](p) - z[m - 1](p0)) ** 2) + \
+                    1 / c[-1] * ((x[m](p) - xF) + Mk * (z[m](p) - zF)) / np.sqrt(
+                        (x[m](p) - xF) ** 2 + (z[m](p) - zF) ** 2)
+
+                Vk = \
+                    1 / c[-2] * ((x[m](p + 1) - x[m - 1](p0)) + Mk * (z[m](p + 1) - z[m - 1](p0))) / np.sqrt(
+                        (x[m](p + 1) - x[m - 1](p0)) ** 2 + (z[m](p + 1) - z[m - 1](p0)) ** 2) + \
+                    1 / c[-1] * ((x[m](p + 1) - xF) + Mk * (z[m](p + 1) - zF)) / np.sqrt(
+                        (x[m](p + 1) - xF) ** 2 + (z[m](p + 1) - zF) ** 2)
+
+                step[-1] = Vk0 / (Vk - Vk0)
+
+            else:
+                p0 = k0[m - 1]
+                p = k0[m]
+                pn = k0[m + 1]
+
+                Mk = (z[m](p + 1) - z[m](p)) / (x[m](p + 1) - x[m](p))
+
+                Vk0 = \
+                    1 / c[m] * ((x[m](p) - x[m - 1](p0)) + Mk * (z[m](p) - z[m - 1](p0))) / np.sqrt(
+                        (x[m](p) - x[m - 1](p0)) ** 2 + (z[m](p) - z[m - 1](p0)) ** 2) + \
+                    1 / c[m + 1] * ((x[m](p) - x[m + 1](pn)) + Mk * (z[m](p) - z[m + 1](pn))) / np.sqrt(
+                        (x[m](p) - x[m + 1](pn)) ** 2 + (z[m](p) - z[m + 1](pn)) ** 2)
+
+                Vk = \
+                    1 / c[m] * ((x[m](p + 1) - x[m - 1](p0)) + Mk * (z[m](p + 1) - z[m - 1](p0))) / np.sqrt(
+                        (x[m](p + 1) - x[m - 1](p0)) ** 2 + (z[m](p + 1) - z[m - 1](p0)) ** 2) + \
+                    1 / c[m + 1] * ((x[m](p + 1) - x[m + 1](pn)) + Mk * (z[m](p + 1) - z[m + 1](pn))) / np.sqrt(
+                        (x[m](p + 1) - x[m + 1](pn)) ** 2 + (z[m](p + 1) - z[m + 1](pn)) ** 2)
+
+                step[m] = Vk0 / (Vk - Vk0)
+
+        k = k0 - step
+        k = np.array([np.max([np.min([k[i], N[i] - 2]), 0]) for i in range(M-1)])
+
+        output['ki'].append(k)
+        output['iter'] += 1
+        #print(k)
+        if np.all(np.abs(k - k0) <= tolerance):
+            output['elapsed_time'] = time.time() - t0
+            output['converged'] = True
+            output['result'] = k
+            break
+        elif i == maxiter-1:
+            output['elapsed_time'] = -1
+            output['converged'] = False
+            output['result'] = k
+        else:
+            k0 = np.copy(k)
+    return output
+
 def parrilla_generalized(x: list, z: list, xA: float, zA: float, xF: float, zF: float, c: list, tolerance: int=2, maxiter: int=100):
     N = [len(xi) for xi in x]
     M = len(c)
--- tests/m_mediums.py
+++ tests/m_mediums.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib
 import time
 
-from src.surface_gpu.methods import parrilla_generalized
+from src.surface_gpu.methods import parrilla_generalized, parrilla_generalized_interpolated
+
 matplotlib.use("TkAgg")
 
 # Speed:
@@ -29,35 +30,46 @@ x1 = x
 z1 = np.sin(.25 * x) + 5
 
 x2 = x
-z2 = x2**2/20 + 10
+z2 = x2 ** 2 / 20 + 10
 
 x3 = x
 z3 = np.sin(.25 * x) + 20
 
+xdiscr = [x1, x2, x3]
+N = [len(xi) for xi in [x1, x2, x3]]
 
-x = [x1, x2, x3]
-z = [z1, z2, z3]
+t1 = lambda x0: np.interp(x0, x1, x1)
+t2 = lambda x0: np.interp(x0, x2, x2)
+t3 = lambda x0: np.interp(x0, x3, x3)
+
+f1 = lambda x0: np.interp(x0, x1, z1)
+f2 = lambda x0: np.interp(x0, x2, z2)
+f3 = lambda x0: np.interp(x0, x3, z3)
+
+x = [t1, t2, t3]
+z = [f1, f2, f3]
 c = [c1, c2, c3, c4]
 M = len(x)
 
-result = parrilla_generalized(x, z, xA, zA, xF, zF, c, tolerance=4, maxiter=200)
+
+result = parrilla_generalized_interpolated(x, z, N, xA, zA, xF, zF, c, tolerance=4, maxiter=200)
 
 k = result['result']
 elapsed_time = result['elapsed_time']
 
 # Plot rays:
-for i in range(M+1):
+for i in range(M + 1):
     if i == 0:
-        plt.plot([xA, x[i][k[0]]], [zA, z[i][k[0]]], color='lime')
+        plt.plot([xA, x[i](k[0])], [zA, z[i](k[0])], color='lime')
     elif i == M:
-        plt.plot([xF, x[i-1][k[i-1]]], [zF, z[i-1][k[i-1]]], color='lime')
+        plt.plot([xF, x[i - 1](k[i - 1])], [zF, z[i - 1](k[i - 1])], color='lime')
     else:
-        plt.plot([x[i][k[i]], x[i-1][k[i-1]]], [z[i][k[i]], z[i-1][k[i-1]]], color='lime')
+        plt.plot([x[i](k[i]), x[i - 1](k[i - 1])], [z[i](k[i]), z[i - 1](k[i - 1])], color='lime')
 
 # Plot surfaces:
 for i in range(M):
-    plt.plot(x[i], z[i], 'o-', color="b", alpha=.5, label=fr"$S_{i + 1}$. $k_{i}={k[i]}$", markersize=2)
-    plt.plot(x[i][k[i]], z[i][k[i]], '*', color="r")
+    plt.plot(x[i](xdiscr[i]), z[i](xdiscr[i]), 'o-', color="b", alpha=.5, label=fr"$S_{i + 1}$. $k_{i}={k[i]}$", markersize=2)
+    plt.plot(x[i](k[i]), z[i](k[i]), '*', color="r")
 
 # Emitter:
 plt.plot(xA, zA, 'sk', label="Emitters")
